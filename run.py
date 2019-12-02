c0784091ee16f16ffd904350425c8478a9fec33e
martakortslaur <martakortslaur@gmail.com>
2019-12-02 17:15:35 +0000

    Heroku

--- run.py
+++ run.py
@@ -4,24 +4,26 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route("/about")
+
+@app.route('/about')
 def about():
-    return render_template("about.html")
+    return render_template("about.html", page_title="About")
 
 
 @app.route('/contact')
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
+
 
 @app.route('/careers')
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Careers")
 
-if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"),
-            port=os.environ.get("PORT"),
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
             debug=True)
